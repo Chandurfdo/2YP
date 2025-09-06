f7468605651eda6f2c95b315b9ccb1d25a5395da
FERNANDO W.W.C.C.J. <e21137@eng.pdn.ac.lk>
2025-09-06 14:06:04 +0530

    added the usernanme

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
+  user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
   database: process.env.DB_NAME || 'organizer_dashboard',
   password: process.env.DB_PASSWORD || 'xxxx',
